e32e79eaccfadc94a102d993ce90bc082481b93e
Sachin Verma <sach786in86verma@gmail.com>
2018-09-23 23:59:02 +0530

    Basic Calculator

---
+++ calc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int add(int x, int y) {
+	return 0;
+}
+
+int sub(int x, int y) {
+	return 0;
+}
+
+int div(int x, int y) {
+	return 0;
+}
+
+int mul(int x, int y) {
+	return 0;
+}
+
+int main()
+{
+	int a1, a2, a3, a4;
+	a1 = add(6, 2);
+	a1 = sub(6, 2);
+	a1 = div(6, 2);
+	a1 = mul(6, 2);
+	return 0;
+}
